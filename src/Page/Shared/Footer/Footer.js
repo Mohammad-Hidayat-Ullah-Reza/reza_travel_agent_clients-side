364765bffd3408e9179f7df3e7cb8ba6c7ae1aee
mohammadHidayat <mohammadhidayat8@gmail.com>
2022-11-10 20:25:19 +0600

    footer updated and react helmet added on all page

--- src/Page/Shared/Footer/Footer.js
+++ src/Page/Shared/Footer/Footer.js
@@ -1,20 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../../assets/logo.png";
 
 const Footer = () => {
   return (
     <footer className="p-4 bg-white sm:p-6 dark:bg-gray-900">
       <div className="md:flex md:justify-between">
         <div className="mb-6 md:mb-0">
-          <a href="https://flowbite.com/" className="flex items-center">
-            <img
-              src="https://flowbite.com/docs/images/logo.svg"
-              className="mr-3 h-8"
-              alt="FlowBite Logo"
-            />
+          <Link to="/" className="flex items-center">
+            <img src={logo} className="mr-3 h-8" alt="Reza Travel Agent Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Flowbite
+              Reza Travel Agent
             </span>
-          </a>
+          </Link>
         </div>
         <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
           <div>
@@ -23,14 +21,14 @@ const Footer = () => {
             </h2>
             <ul className="text-gray-600 dark:text-gray-400">
               <li className="mb-4">
-                <a href="https://flowbite.com/" className="hover:underline">
-                  Flowbite
-                </a>
+                <Link to="/blog" className="hover:underline">
+                  Blog
+                </Link>
               </li>
               <li>
-                <a href="https://tailwindcss.com/" className="hover:underline">
-                  Tailwind CSS
-                </a>
+                <Link to="/services" className="hover:underline">
+                  Services
+                </Link>
               </li>
             </ul>
           </div>
@@ -40,18 +38,12 @@ const Footer = () => {
             </h2>
             <ul className="text-gray-600 dark:text-gray-400">
               <li className="mb-4">
-                <a
-                  href="https://github.com/themesberg/flowbite"
-                  className="hover:underline "
-                >
+                <a href="https://github.com/" className="hover:underline ">
                   Github
                 </a>
               </li>
               <li>
-                <a
-                  href="https://discord.gg/4eeurUVvTy"
-                  className="hover:underline"
-                >
+                <a href="https://discord.gg/" className="hover:underline">
                   Discord
                 </a>
               </li>
@@ -63,14 +55,12 @@ const Footer = () => {
             </h2>
             <ul className="text-gray-600 dark:text-gray-400">
               <li className="mb-4">
-                <a href="/" className="hover:underline">
-                  Privacy Policy
-                </a>
+                <button className="hover:underline">Privacy Policy</button>
               </li>
               <li>
-                <a href="/" className="hover:underline">
+                <button className="hover:underline">
                   Terms &amp; Conditions
-                </a>
+                </button>
               </li>
             </ul>
           </div>
@@ -80,14 +70,14 @@ const Footer = () => {
       <div className="sm:flex sm:items-center sm:justify-between">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2022{" "}
-          <a href="https://flowbite.com/" className="hover:underline">
-            Flowbite™
-          </a>
+          <Link to="/" className="hover:underline">
+            REZA TRAVEL AGENT™
+          </Link>
           . All Rights Reserved.
         </span>
         <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
           <a
-            href="/"
+            href="https://www.facebook.com/"
             className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
           >
             <svg
@@ -105,7 +95,7 @@ const Footer = () => {
             <span className="sr-only">Facebook page</span>
           </a>
           <a
-            href="/"
+            href="https://www.instagram.com/"
             className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
           >
             <svg
@@ -123,7 +113,7 @@ const Footer = () => {
             <span className="sr-only">Instagram page</span>
           </a>
           <a
-            href="/"
+            href="https://twitter.com/"
             className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
           >
             <svg
@@ -137,7 +127,7 @@ const Footer = () => {
             <span className="sr-only">Twitter page</span>
           </a>
           <a
-            href="/"
+            href="https://github.com/"
             className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
           >
             <svg
@@ -155,7 +145,7 @@ const Footer = () => {
             <span className="sr-only">GitHub account</span>
           </a>
           <a
-            href="/"
+            href="https://dribbble.com/"
             className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
           >
             <svg
